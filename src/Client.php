d11293105f9589123d8d1067ce5cf3cae6c54c83
musayann <musayann@octangrp.com>
2021-10-04 21:39:21 +0200

    feat: add authorize methods

--- src/Client.php
+++ src/Client.php
@@ -4,53 +4,35 @@ namespace LNDHub;
 
 use GuzzleHttp;
 
-class Client {
-
-  private $address = 'https://lnbits.com';
-  private $apiKey;
+class Client
+{
+  
   private $client;
+  private $access_token;
+  private $refresh_token;
+  private $url;
+  private $login;
+  private $password;
 
-  public function __construct($apiKey) {
-    $this->apiKey = $apiKey;
+  public function __construct($url, $login, $password)
+  {
+    $this->url = $url;
+    $this->login = $login;
+    $this->password = $password;
   }
 
-  public function setAddress($address) {
-    $this->address = $address;
+  public function init()
+  {
+    return $this->authorize();
   }
 
-  public function isConnectionValid() {
-    return !empty($this->address) && !empty($this->apiKey);
-  }
-
-  public function getInfo() {
-    $info = $this->request('GET', '/api/v1/wallet');
-    $info['alias'] = $info['name'];
-    $info['identity_pubkey'] = 'LNBits: ' . $info['id'];
-    return $info;
-  }
-
-  public function addInvoice($invoice) {
-    $requestBody = [ "out" => false, "amount" => $invoice['value'], "memo" => $invoice['memo'] ];
-    $invoice = $this->request('POST', '/api/v1/payments', json_encode($requestBody));
-    $invoice['r_hash'] = $invoice['checking_id']; // kinda mimic lnd
-    return $invoice;
-  }
-
-  public function getInvoice($checkingId) {
-    $invoice = $this->request('GET', '/api/v1/payments/' . $checkingId);
-    $invoice['settled'] = $invoice['paid']; //kinda mimic lnd
-    return $invoice;
-  }
-
-  public function isInvoicePaid($checkingId) {
-    $invoice = $this->getInvoice($checkingId);
-    return $invoice['settled'];
-  }
-
-  private function request($method, $path, $body = null) {
+  private function request($method, $path, $body = null)
+  {
     $headers = [
-      'X-Api-Key' => $this->apiKey,
-      'Content-Type' => 'application/json'
+      'Accept' => 'application/json',
+      'Content-Type' => 'application/json',
+      'Access-Control-Allow-Origin' => '*',
+      'Authorization' => "Bearer {$this->access_token}"
     ];
 
     $request = new GuzzleHttp\Psr7\Request($method, $path, $headers, $body);
@@ -63,14 +45,54 @@ class Client {
     }
   }
 
-  private function client() {
+  private function authorize()
+  {
+    $headers = [
+      'Accept' => 'application/json',
+      'Content-Type' => 'application/json',
+      'Access-Control-Allow-Origin' => '*'
+    ];
+    $body = ["login" => $this->login, "password" => $this->password];
+    $request = new GuzzleHttp\Psr7\Request('POST', '/auth?type=auth', $headers, json_encode($body));
+    $response = $this->client()->send($request);
+    if ($response->getStatusCode() >= 200 && $response->getStatusCode() < 300) {
+      $responseBody = $response->getBody()->getContents();
+      $data = json_decode($responseBody, true);
+      $this->access_token = $data->access_token;
+      $this->refresh_token = $data->refresh_token;
+      return $data;
+    } else {
+      // raise exception
+    }
+  }
+
+  private function getInfo()
+  {
+    $data = $this->request("GET", "/getinfo", []);
+    return [
+      "data" => [
+        "alias" => $data->alias
+      ]
+    ];
+  }
+
+  private function getBalance()
+  {
+    $data = $this->request("GET", "/balance", []);
+    return [
+      "data" => [
+        "balance" => $data->BTC->AvailableBalance
+      ]
+    ];
+  }
+
+  private function client()
+  {
     if ($this->client) {
       return $this->client;
     }
-    $options = ['base_uri' => $this->address];
+    $options = ['base_uri' => $this->url];
     $this->client = new GuzzleHttp\Client($options);
     return $this->client;
   }
 }
-
-?>
